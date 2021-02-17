01f0a661e6af4a950b03c866e9c03d2e79671aa6
yoshi-automation <yoshi-automation@google.com>
2021-02-17 04:54:40 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- .jsdoc.js
+++ .jsdoc.js
@@ -43,7 +43,7 @@ module.exports = {
     copyright: 'Copyright 2021 Google LLC',
     includeDate: false,
     sourceFiles: false,
-    systemName: '@google-cloud/network-connectivity',
+    systemName: '@google-cloud/networkconnectivity',
     theme: 'lumen',
     default: {
       outputSourceFiles: false
